5aae1faa1fb2f91f65a3e8679baabc0e2b0f2899
labsfiap <labsfiap@L1710MICRO56>
2025-09-03 22:06:48 -0300

    Add project files.

---
+++ performance-cache/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using MySqlConnector;
+using System.Threading.Tasks;
+using performance_cache.Model;
+
+namespace performance_cache.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            using var connection = new MySqlConnection("Server=localhost;database=fiap;User=root;Password=123");
+            await connection.OpenAsync();
+            string sql = "select id, name, email from users;";
+            var users = await connection.QueryAsync<Users>(sql);
+            return Ok(users);
+        }
+    }
+}
